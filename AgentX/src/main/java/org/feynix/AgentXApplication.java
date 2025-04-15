59ccedc181f187dc4f5238818c128a0a062e136d
Feynix <1048218204@qq.com>
2025-04-15 19:58:32 +0800

    feature : 开启x-file-storage配置

--- AgentX/src/main/java/org/feynix/AgentXApplication.java
+++ AgentX/src/main/java/org/feynix/AgentXApplication.java
@@ -1,13 +1,14 @@
 package org.feynix;
 
+import org.dromara.x.file.storage.spring.EnableFileStorage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.feynix.infrastructure.utils.PasswordUtils;
 
-/** 应用入口类 */
+/**
+ * 应用入口类
+ */
 @SpringBootApplication
-@EnableScheduling
+@EnableFileStorage
 public class AgentXApplication {
 
     public static void main(String[] args) {
