b7df3469cf63d1657738e0b9157b0ea09312aae9
Feynix <1048218204@qq.com>
2025-04-24 10:54:12 +0800

    feature : 文件上传接口调试适配S3协议

---
+++ AgentX/src/main/java/org/feynix/application/rag/assembler/FileAssembler.java
@@ -0,0 +1,29 @@
+package org.feynix.application.rag.assembler;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import org.feynix.domain.agent.constant.AgentType;
+import org.feynix.domain.agent.model.AgentEntity;
+import org.feynix.domain.rag.model.FileDetailEntity;
+import org.feynix.interfaces.dto.agent.CreateAgentRequest;
+import org.feynix.interfaces.dto.rag.RagUploadRequest;
+
+/**
+ * @author shilong.zang
+ * @date 11:09 <br/>
+ */
+public class FileAssembler {
+
+    /**
+     * 将CreateAgentRequest转换为AgentEntity
+     */
+    public static FileDetailEntity toEntity(RagUploadRequest request) {
+
+        FileDetailEntity fileDetailEntity = new FileDetailEntity();
+        fileDetailEntity.setMultipartFile(request.getFile());
+        fileDetailEntity.setDataSetId(request.getDataSetId());
+
+        return fileDetailEntity;
+    }
+}
---
+++ AgentX/src/main/java/org/feynix/application/rag/service/FileService.java
@@ -0,0 +1,31 @@
+package org.feynix.application.rag.service;
+
+
+import org.springframework.stereotype.Service;
+import org.feynix.application.rag.assembler.FileAssembler;
+import org.feynix.domain.rag.model.FileDetailEntity;
+import org.feynix.domain.rag.service.FileDetailService;
+import org.feynix.interfaces.dto.rag.RagUploadRequest;
+
+/**
+ * @author shilong.zang
+ * @date 15:43 <br/>
+ */
+
+@Service
+public class FileService {
+
+    private final FileDetailService fileDetailService;
+
+    public FileService(FileDetailService fileDetailService) {
+        this.fileDetailService = fileDetailService;
+    }
+
+
+    public void upload(RagUploadRequest request) {
+
+        final FileDetailEntity fileDetailEntity = FileAssembler.toEntity(request);
+
+        fileDetailService.upload(fileDetailEntity);
+    }
+}
