e87069fdcf5f11dd8bf8ad7af16e8a37311930f4
Feynix <1048218204@qq.com>
2025-05-24 13:30:18 +0800

    build：使用快照 langchain4j 替代上游 langchain4j
    - 支持调用工具前预先设置工具参数

--- AgentX/src/main/java/org/feynix/infrastructure/llm/LLMProviderService.java
+++ AgentX/src/main/java/org/feynix/infrastructure/llm/LLMProviderService.java
@@ -1,18 +1,18 @@
 package org.feynix.infrastructure.llm;
 
-import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.chat.StreamingChatLanguageModel;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.StreamingChatModel;
 import org.feynix.infrastructure.llm.config.ProviderConfig;
 import org.feynix.infrastructure.llm.factory.LLMProviderFactory;
 import org.feynix.infrastructure.llm.protocol.enums.ProviderProtocol;
 
 public class LLMProviderService {
 
-    public static ChatLanguageModel getStrand(ProviderProtocol protocol, ProviderConfig providerConfig) {
+    public static ChatModel getStrand(ProviderProtocol protocol, ProviderConfig providerConfig) {
         return LLMProviderFactory.getLLMProvider(protocol, providerConfig);
     }
 
-    public static StreamingChatLanguageModel getStream(ProviderProtocol protocol, ProviderConfig providerConfig) {
+    public static StreamingChatModel getStream(ProviderProtocol protocol, ProviderConfig providerConfig) {
         return LLMProviderFactory.getLLMProviderByStream(protocol, providerConfig);
     }
 }
