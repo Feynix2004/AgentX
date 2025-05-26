f5f1b0a5ed56fe87670d4a585343e8a7af4afe6e
Feynix <1048218204@qq.com>
2025-05-26 21:11:42 +0800

    code fix

--- AgentX/src/main/java/org/feynix/domain/scheduledtask/repository/ScheduledTaskRepository.java
+++ AgentX/src/main/java/org/feynix/domain/scheduledtask/repository/ScheduledTaskRepository.java
@@ -4,9 +4,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.feynix.domain.scheduledtask.model.ScheduledTaskEntity;
 import org.feynix.infrastructure.repository.MyBatisPlusExtRepository;
 
-/**
- * 定时任务仓储接口
- */
+/** 定时任务仓储接口 */
 @Mapper
 public interface ScheduledTaskRepository extends MyBatisPlusExtRepository<ScheduledTaskEntity> {
-} 
+}
