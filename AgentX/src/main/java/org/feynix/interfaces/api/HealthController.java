aab110a62cfb215ca5439b9221a9b61d76a720a1
Feynix <1048218204@qq.com>
2025-06-14 10:25:57 +0800

    fix

--- AgentX/src/main/java/org/feynix/interfaces/api/HealthController.java
+++ AgentX/src/main/java/org/feynix/interfaces/api/HealthController.java
@@ -1,6 +1,5 @@
 package org.feynix.interfaces.api;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +9,8 @@ import org.feynix.interfaces.api.common.Result;
 @RestController
 public class HealthController {
 
-
     @GetMapping("/health")
-    public Result<Object> health(){
+    public Result<Object> health() {
         return Result.success().message("ok");
     }
 }
