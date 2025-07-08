51d3ebbb93d748eb95f54e153ede095a0097f15e
Feynix <1048218204@qq.com>
2025-07-08 22:43:32 +0800

    解决重复 s3 依赖

--- AgentX/src/main/java/org/feynix/AgentXApplication.java
+++ AgentX/src/main/java/org/feynix/AgentXApplication.java
@@ -1,6 +1,5 @@
 package org.feynix;
 
-import org.dromara.x.file.storage.spring.EnableFileStorage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
